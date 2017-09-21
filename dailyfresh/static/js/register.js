548781817fb793611e0561176810045f805689c9
gyczxc <874746615@qq.com>
2017-09-21 21:23:31 +0800

    这是2017-9-21 21.23提交的

--- dailyfresh/static/js/register.js
+++ dailyfresh/static/js/register.js
@@ -42,15 +42,28 @@ $(function(){
 		var len = $('#user_name').val().length;
 		if(len<5||len>20)
 		{
-			$('#user_name').next().html('请输入5-20个字符的用户名')
+			$('#user_name').next().html('请输入-----5-20个字符的用户名111')
 			$('#user_name').next().show();
 			error_name = true;
+			$.alert('sdaf')
 		}
 		else
 		{
-			$('#user_name').next().hide();
-			error_name = false;
+			// $.get('/user/register_exist/?uname='+$('#user_name').val(),function (data) {
+			uname = $('#user_name').val()
+			$.get('/user/register_exist/?uname='+uname,function(){
+				$.alert('--------------')
+		            		if(data.count==1){
+		            		    $('#user_name').next().html('用户名已存在').show()
+							error_name = true;
+				}else{
+					$('#user_name').next().hide();
+                	error_name = false;
+				}
+            });
+
 		}
+
 	}
 
 	function check_pwd(){
